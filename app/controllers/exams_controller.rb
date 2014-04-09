8978f99bf25b2bdee8965e4c8c6b9b32d811e155
Muralee Mohan <muraleem3@gmail.com>
2014-04-09 18:43:38 +0530

    exam controller

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,2 +1,79 @@
 class ExamsController < ApplicationController
+	before_action :set_exam, only: [ :show, :edit, :update, :destroy]
+
+	def index
+    if params[:dept].present? && params[:dept] != "AllDepts"
+        @exams = Exam.where(:dept => params[:dept])
+      else
+         @exams = Exam.all.order("date")
+     end
+	end
+  def index1
+    if params[:dept].present? && params[:dept] != "AllDepts"
+        @exams = Exam.where(:dept => params[:dept])
+      else
+         @exams = Exam.all.order("date")
+     end
+  end
+  def show
+    
+  end
+
+	def new
+        @exam=Exam.new
+    end
+
+    def create
+      @exam = Exam.new(exam_params)
+
+      respond_to do |format|
+        if @exam.save
+          format.html { redirect_to @exam, notice: 'Exam Schedule was successfully created.' }
+          #format.json { render action: 'show', status: :created, location: @exam }
+        else
+          format.html { render action: 'new' }
+          #format.json { render json: @exam.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def edit
+        
+    end
+
+    def update
+        
+        respond_to do |format|
+            if @exam.update(exam_params)
+                format.html { redirect_to @exam, notice: 'Exam Schedule was successfully updated.' }
+                format.json { head :no_content }
+            else
+                format.html { render action: 'edit' }
+                #format.json { render json: @exam.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+
+    def destroy
+        
+        @exam.destroy
+        respond_to do |format|
+          format.html { redirect_to exams_url }
+          format.json { head :no_content }
+        end
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+        def set_exam
+          @exam = Exam.find(params[:id])
+        end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+        def exam_params
+          params.require(:exam).permit(:dept, :semester, :subject, :date, :from, :to)
+        end
+
+
 end
