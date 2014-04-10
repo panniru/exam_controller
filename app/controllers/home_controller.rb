9b3ca7914418317a22b8d4f3af308ab007272458
Priya14 <shanmugapriya141292@gmail.com>
2014-04-10 17:41:18 +0530

    welcome and notification modified

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_authorization_check
   def home_page
     @welcome = Welcome.first
-    @notifications = Notification.all.order("created_at DESC").limit(5)
+    @notifications = Notification.all.order("created_at DESC") #.limit(5)
     @images = Document.where(:file_type => 'image')
   end
 end
