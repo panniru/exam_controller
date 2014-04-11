298a38993e7e6120665b60a084f857d3fd0b6ba8
Srikanth <sri.panniru08@gmail.com>
2014-04-11 22:40:01 +0530

    hall of fames secounf cut

--- app/controllers/hall_of_fames_controller.rb
+++ app/controllers/hall_of_fames_controller.rb
@@ -1,10 +1,15 @@
 class HallOfFamesController < ApplicationController
+  load_resource :only => [:show, :update, :edit, :destroy, :new]
+  skip_authorization_check 
+  
   def index
     @hall_of_fames =HallOfFame.all
   end
+  
   def new
     @hall_of_fame = HallOfFame.new
   end
+  
   def create
     @hall_of_fame = HallOfFame.new(hall_of_fame_params)
     if @hall_of_fame.save
@@ -15,11 +20,36 @@ class HallOfFamesController < ApplicationController
       render "new"
     end
   end
+  
   def show
-      @hall_of_fame = HallOfFame.find(params[:id])
+    @hall_of_fame = HallOfFame.find(params[:id])
   end
+
+  def update
+    if @hall_of_fame.update(hall_of_fame_params)
+      flash.now[:success] = I18n.t :success, :scope => [:hall_of_fame, :update]
+      redirect_to hall_of_fames_path
+    else
+      flash.now[:fail] = I18n.t :fail, :scope => [:hall_of_fame, :update]
+      render "edit"
+    end
+  end
+
+  def destroy
+    if @hall_of_fame.destroy
+      flash.now[:success] = I18n.t :success, :scope => [:hall_of_fame, :destroy]
+      redirect_to hall_of_fames_path
+    else
+      flash.now[:fail] = I18n.t :fail, :scope => [:student, :destroy]
+      render "show"
+    end
+  end
+
+  private
+  
   def hall_of_fame_params
     params.require(:hall_of_fame).permit(:name, :department, :year_of_passing, :avatar)
   end
+
 end
 
