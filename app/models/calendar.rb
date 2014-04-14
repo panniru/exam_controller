5a564e376825d1091487869a1ca3f56219a10e34
Muralee Mohan <muraleem3@gmail.com>
2014-04-14 13:13:39 +0530

    exams and calendar custom scroll

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,8 +1,9 @@
 class Calendar < ActiveRecord::Base
 
-	validates :course, :presence => true
-
-	def self.grouped_calendars
-  	Calendar.select(:course).group(:course)
-	end
+  validates :course, :presence => true
+  validates :from, :presence => true
+  validates :to, :presence => true
+  def self.grouped_calendars
+    Calendar.select(:course).group(:course)
+  end
 end
